b3e2cba4b92b6fb8a4ddeed28e6114ad2eda94d0
Manson <manson972@yahoo.com>
2023-04-19 10:38:21 +0800

    ig story update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,23 @@
 import IGHeader from "components/IGHeader";
 import IGContainer from "components/IGContainer";
 
+import IGStory from "./components/IGStory";
+
 const Home: React.FC = () => {
   return (
     <>
       <IGHeader />
       <IGContainer>
-        HELLO
+        <div className="flex lg:justify-center">
+          {/* left */}
+          <div className="w-full lg:w-[600px]">
+            {" "}
+            {/*超過lg寬度時變成600px，讓右邊顯示 */}
+            <IGStory />
+          </div>
+          {/* right */}
+          <div className="hidden lg:block lg-w-[424px]">i am right</div>
+        </div>
       </IGContainer>
     </>
   );
