7ae2da13141ada31a6f8b55300875c7e8693f9cd
Manson <manson972@yahoo.com>
2023-04-19 09:11:02 +0800

    ig container update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,13 @@
 import IGHeader from "components/IGHeader";
+import IGContainer from "components/IGContainer";
 
 const Home: React.FC = () => {
   return (
     <>
       <IGHeader />
+      <IGContainer>
+        HELLO
+      </IGContainer>
     </>
   );
 };
