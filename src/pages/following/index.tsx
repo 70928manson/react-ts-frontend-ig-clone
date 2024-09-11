dec5c6804c820145af5a7a8bea9b74bd9194dd54
70928manson <manson972@yahoo.com>
2024-09-11 16:20:27 +0800

    style: 刪除未用到的註解

--- src/pages/following/index.tsx
+++ src/pages/following/index.tsx
@@ -7,36 +7,6 @@ import { useAppSelector } from "hooks";
 const Following: React.FC = () => {
   const friendReducer = useAppSelector((state) => state.friendReducer);
   const friends = friendReducer.friends;
-  // const friends = [
-  //   {
-  //     id: 1,
-  //     location: "Singapore",
-  //     account: "max_999",
-  //     isFollowing: true,
-  //     avatar: "/images/avatars/a1.png",
-  //   },
-  //   {
-  //     id: 2,
-  //     location: "Taiwan",
-  //     account: "fm_999",
-  //     isFollowing: true,
-  //     avatar: "/images/avatars/a2.png",
-  //   },
-  //   {
-  //     id: 3,
-  //     location: "USA",
-  //     account: "joanne_999",
-  //     isFollowing: true,
-  //     avatar: "/images/avatars/a3.png",
-  //   },
-  //   {
-  //     id: 4,
-  //     location: "Japan",
-  //     account: "focus_999",
-  //     isFollowing: true,
-  //     avatar: "/images/avatars/a4.png",
-  //   },
-  // ];
 
   return (
     <>
--- src/pages/home/components/IGProfile/index.tsx
+++ src/pages/home/components/IGProfile/index.tsx
@@ -3,38 +3,7 @@ import { useAppSelector } from "hooks";
 
 const IGProfile: React.FC = () => {
   const friendReducer = useAppSelector((state) => state.friendReducer);
-  const friends = friendReducer.friends.slice(0, 4);
-
-  // const friends = [
-  //       {
-  //         id: 1,
-  //         location: "Singapore",
-  //         account: "max_999",
-  //         isFollowing: true,
-  //         avatar: "/images/avatars/a1.png",
-  //       },
-  //       {
-  //         id: 2,
-  //         location: "Taiwan",
-  //         account: "fm_999",
-  //         isFollowing: true,
-  //         avatar: "/images/avatars/a2.png",
-  //       },
-  //       {
-  //         id: 3,
-  //         location: "USA",
-  //         account: "joanne_999",
-  //         isFollowing: true,
-  //         avatar: "/images/avatars/a3.png",
-  //       },
-  //       {
-  //         id: 4,
-  //         location: "Japan",
-  //         account: "focus_999",
-  //         isFollowing: true,
-  //         avatar: "/images/avatars/a4.png",
-  //       },
-  //   ];
+  const friends = friendReducer.friends.slice(0, 5);
 
   return (
     <div className="mt-8 ml-8 shadow-lg box-border p-2">
