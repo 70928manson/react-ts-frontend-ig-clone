bebdfec9815ff0649e46af9385dfa77bb5994b6c
70928manson <manson972@yahoo.com>
2024-09-11 14:34:48 +0800

    feat: 限時動態大頭照採用 lazy loading

--- src/pages/home/components/IGStory/Item.tsx
+++ src/pages/home/components/IGStory/Item.tsx
@@ -1,5 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 type ItemProps = {
   name: string;
   avatar: string;
@@ -13,15 +16,24 @@ const Item: React.FC<ItemProps> = ({ name, avatar, id }) => {
     <div className="text-center">
       <div
         className="w-[56px] h-[56px] p-[3px] ring-2 border-2 border-white ring-red-500 rounded-full mx-[11px] overflow-hidden cursor-pointer"
-        style={{
-          backgroundImage: `url(${avatar})`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-        }}
         onClick={() => {
           go(`/react-ts-frontend-ig-clone/stories/${id}`);
         }}
-      ></div>
+      >
+        <LazyLoadImage
+          src={avatar}
+          alt={name}
+          effect="blur"         // 加載前模糊
+          width="100%"
+          height="100%"
+          style={{
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            objectFit: "cover", // 確保圖片適應容器
+            display: "block",   // 避免圖片 inline-block 的問題
+          }}
+        />
+      </div>
       <p className="text-xs mt-1">{name}</p>
     </div>
   );
