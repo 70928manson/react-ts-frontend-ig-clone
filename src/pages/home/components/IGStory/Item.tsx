33847654f0b13d470ba9c0137f2c02cad6ec1611
70928manson <manson972@yahoo.com>
2024-09-12 11:41:44 +0800

    fix: 修正 LazyLoadImage UI 跑版
    - LazyLoadImage width, height 設定為 100% 會使圖片按照原大小比例顯示, 需要用 style 設定 100% 讓他依照 img 上一層的容器

--- src/pages/home/components/IGStory/Item.tsx
+++ src/pages/home/components/IGStory/Item.tsx
@@ -14,26 +14,23 @@ const Item: React.FC<ItemProps> = ({ name, avatar, id }) => {
 
   return (
     <div className="text-center">
-      <div
-        className="w-[56px] h-[56px] p-[3px] ring-2 border-2 border-white ring-red-500 rounded-full mx-[11px] overflow-hidden cursor-pointer"
+      <LazyLoadImage
+        src={avatar}
+        alt={name}
+        effect="blur"         // 加載前模糊
+        wrapperClassName="w-[56px] h-[56px] ring-2 border-2 border-white ring-red-500 rounded-full mx-[11px] overflow-hidden cursor-pointer"
+        style={{
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          objectFit: "cover", // 確保圖片適應容器
+          display: "block",   // 避免圖片 inline-block 的問題
+          width: "100%",
+          height: "100%",
+        }}
         onClick={() => {
           go(`/react-ts-frontend-ig-clone/stories/${id}`);
         }}
-      >
-        <LazyLoadImage
-          src={avatar}
-          alt={name}
-          effect="blur"         // 加載前模糊
-          width="100%"
-          height="100%"
-          style={{
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            objectFit: "cover", // 確保圖片適應容器
-            display: "block",   // 避免圖片 inline-block 的問題
-          }}
-        />
-      </div>
+      />
       <p className="text-xs mt-1">{name}</p>
     </div>
   );
